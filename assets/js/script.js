d4d2a79dc0906c7e3c4e6477ad19b7168880989d
benaicha <benaichamohammed@outlook.fr>
2023-10-25 15:55:45 +0200

    commit-mercredi-16h

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
 console.log("script chargée !");
+
+
+// Méthode avec fetch
+
+let datasFetch;
+const urlApi = "https://pokebuildapi.fr/api/v1/pokemon/limit/100";
+await getDataFetch();
+async function getDataFetch () {
+    const res = await fetch(urlApi);
+    datasFetch = await res.json();
+}
+
+console.log(datasFetch);
+
+// datasFetch.forEach(pokemon => {
+//     let optionPokemon = document.createElement("option");
+//     optionPokemon.innerText = pokemon.name;
+//     document.querySelector('.menu-select').appendChild(optionPokemon);
+// });
