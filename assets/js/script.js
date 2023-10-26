f3fb18085f6ac84b00224a90bd7714e93220f68c
benaicha <benaichamohammed@outlook.fr>
2023-10-26 13:40:41 +0200

    jeudi 13h40

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ console.log("script chargée !");
 
 
 // Méthode avec fetch
-
+// avec cette methode j'interoge l'api et je recupere des info
 let datasFetch;
 const urlApi = "https://pokebuildapi.fr/api/v1/pokemon/limit/100";
 await getDataFetch();
@@ -10,11 +10,32 @@ async function getDataFetch () {
     const res = await fetch(urlApi);
     datasFetch = await res.json();
 }
-
 console.log(datasFetch);
+datasFetch.forEach(pokemon => {
+    let optionPokemon = document.createElement("option");
+    optionPokemon.innerText = pokemon.name;
+    document.querySelector('.menu-select').appendChild(optionPokemon);
+ 
+        
+    });
+// menuSelectNamePoke.addEventListener("change", function () {
+//         let pokemonDemande = dataFetch.find((dataFetchPokemon) => dataFetchPokemon.name == menuSelectNamePoke.value);
+//         boiteImagePokemon.innerHTML = "";
+//         let imagePokemon = document.createElement("img");
+//         imagePokemon.src = pokemonDemande.image;
+//         console.log(imagePokemon);
+//         boiteImagePokemon.appendChild(imagePokemon);
+//     })
+   
+    // let menuselect = document.addEventListener("change", function () {
+    //     let pokedemande = datasFetch.find(datasFetchpokemon) => datasFetchPokemon.name == menuSelect.value);
+    //     boiteImagePokemon.innerHTML = "";
 
-// datasFetch.forEach(pokemon => {
-//     let optionPokemon = document.createElement("option");
-//     optionPokemon.innerText = pokemon.name;
-//     document.querySelector('.menu-select').appendChild(optionPokemon);
-// });
+
+        
+    // });
+    // menuselect.classList.add("classe-menu")
+
+
+
+    
