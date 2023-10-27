27910806b943c8f1e219d970bc40f983c7bf35d4
benaicha <benaichamohammed@outlook.fr>
2023-10-27 12:00:43 +0200

    toute est fracasser

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,14 @@ console.log("script chargée !");
 
 // Méthode avec fetch
 // avec cette methode j'interoge l'api et je recupere des info
-let datasFetch;
-const urlApi = "https://pokebuildapi.fr/api/v1/pokemon/limit/100";
+
+
+// je cree une variable 
+let getAllPokemons;
+
+// cette ligne creer une constante urlApi contenant L'URL de L'API qui sera interogée pour obtenir des informatoion sur pikatchu,je peut changer "pikatchu par n'import quelle autre nom de pokemon pour obtenir des "
+const urlApi = "https://pokebuildapi.fr/api/v1/pokemon/pikachu";
+
 await getDataFetch();
 async function getDataFetch () {
     const res = await fetch(urlApi);
@@ -14,18 +20,18 @@ console.log(datasFetch);
 datasFetch.forEach(pokemon => {
     let optionPokemon = document.createElement("option");
     optionPokemon.innerText = pokemon.name;
-    document.querySelector('.menu-select').appendChild(optionPokemon);
+    document.querySelector('menu-select').appendChild(optionPokemon);
  
         
     });
 // menuSelectNamePoke.addEventListener("change", function () {
-//         let pokemonDemande = dataFetch.find((dataFetchPokemon) => dataFetchPokemon.name == menuSelectNamePoke.value);
-//         boiteImagePokemon.innerHTML = "";
-//         let imagePokemon = document.createElement("img");
-//         imagePokemon.src = pokemonDemande.image;
-//         console.log(imagePokemon);
-//         boiteImagePokemon.appendChild(imagePokemon);
-//     })
+    //     let pokemonDemande = dataFetch.find((dataFetchPokemon) => dataFetchPokemon.name == menuSelectNamePoke.value);
+    //     boiteImagePokemon.innerHTML = "";
+    //     let imagePokemon = document.createElement("img");
+    //     imagePokemon.src = pokemonDemande.image;
+    //     console.log(imagePokemon);
+    //     boiteImagePokemon.appendChild(imagePokemon);
+    // })
    
     // let menuselect = document.addEventListener("change", function () {
     //     let pokedemande = datasFetch.find(datasFetchpokemon) => datasFetchPokemon.name == menuSelect.value);
